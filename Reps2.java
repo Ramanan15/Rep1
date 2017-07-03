143acb2fd716b1e8231846bbfd9870c55c7829ef
Ramanan15 <billaramana15@gmail.com>
2017-07-03 10:44:18 +0530

    Add files via upload

---
+++ Reps2.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Reps2{
+	public static void main(String[] args)throws Exception
+	{
+		Scanner in=new Scanner(System.in);
+		int no=in.nextInt();
+		int arr[]=new int[n];
+			for(i=0;i<n0;i++){
+				arr[i]=in.nextInt();
+			}
+			for(i=0;i<n;i++){
+				if(ar[i]==i){
+					System.out.println("the number"+ar[i]+"is equals to its index"+i);
+				}
+			}
+	}
+}
