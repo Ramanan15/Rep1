747b7af534eb9d82e633c8eb8ee156a047a2c4c3
Ramanan15 <billaramana15@gmail.com>
2017-07-04 10:02:31 +0530

    Add files via upload

---
+++ HeigthCalc.java
@@ -0,0 +1,45 @@
+import java.util.Arrays;
+
+
+import java.util.Scanner;
+public class HeightCalc
+{
+
+	public static void main(String[] args)
+	{
+		
+
+		Scanner in=new Scanner(System.in);
+		int[] arr=new int[50]
+;
+		System.out.println("Enter the 50 height values:");
+		for(int i=0;i<arr.length;i++){
+		arr[i]=in.nextInt();
+		}
+		Arrays.sort(arr);
+
+		int arr1[]=new int[arr.length];
+
+		int count=0;
+
+			for(int i=arr.length-1;i<arr.length;i--){
+
+			    arr1[count]=arr[i];
+    
+			    count++;
+		
+
+				if(i<=0){
+
+					break;
+				
+				}
+
+			}
+		System.out.println("The 4th height is->"+arr1[4]);
+		System.out.println("Enter the k value");
+
+		System.out.println(arr1[k]);		
+	}
+
+}
